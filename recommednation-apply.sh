3182dd6ec44893c6e546abf6d9a1af04fb11b05b
Ashok Das <sad.kohsa@gmail.com>
2022-10-17 16:51:45 +0530

    idle file added

--- recommednation-apply.sh
+++ recommednation-apply.sh
@@ -10,7 +10,8 @@ read RECOMMENDED < ${RECOMMEDNATION}
 if [[ ${RECOMMENDED} = CHANGE_MACHINE_TYPE ]]
 then
     echo "${RECOMMENDED}"
-    sed -n '/Resource Name:/,/Action:/{/Resource Name:/!{/Action:/!p}}' file.config > resources.log
+    sed -n '/Resource Name:/,/Machine Type:/{/Resource Name:/!{/Machine Type:/!p}}' file.config > resources.log
+    sed -n '/Machine Type:/,/Location:/{/Machine Type:/!{/Location:/!p}}' file.config > machinetype.log
     while read -r RESOURCE
     do
         RESOURCE_NAME="${RESOURCE}"
@@ -19,7 +20,16 @@ then
             IGNORE_LIST="${IGNORE_RESOURCES}"
             if [[ ${RESOURCE_NAME} != ${IGNORE_LIST} ]]
             then
-
+                sed -n '/Machine Type:/,/Location:/{/Machine Type:/!{/Location:/!p}}' file.config > machinetype.log
+                while read -r MACHINE
+                do
+                    MACHINETYPE="${MACHINE}"
+                    echo "Resource Name: ${RESOURCE_NAME}"
+                    echo "Machine Type: ${MACHINETYPE}"
+                    printf 'yes' | gcloud compute instances stop $RESOURCE_NAME --zone=us-east1-b
+                    printf 'yes' | gcloud compute instances set-machine-type $RESOURCE_NAME --machine-type $MACHINETYPE --zone=us-east1-b
+                    printf 'yes' | gcloud compute instances start $RESOURCE_NAME --zone=us-east1-b
+                done < machinetype.log
             fi 
         done < ignore_list.log
     done < resource.log 
